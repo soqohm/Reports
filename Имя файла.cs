c27ed64a6c7204f3f3bae4a19935e2078a6c357d
soqohm <soqohm@yandex.ru>
2021-10-05 06:58:32 +0500

    Reports

---
+++ Имя файла.cs
@@ -0,0 +1,25 @@
+﻿using TFlex.DOCs.Model.Macros;
+using TFlex.Model.Technology.Macros.ObjectModel;
+
+public class TechnologicalMapsName : MacroProvider
+{
+    public TechnologicalMapsName(MacroContext context)
+        : base(context)
+    {
+    }
+
+    public override void Run()
+    {
+    }
+
+    public string ПолучитьИмяФайлаОтчета()
+    {
+        var объект = ТекущийОбъект;
+
+        if (!объект.Тип.ПорожденОт("Базовый технологический процесс"))
+            return "[пустой отчет - объект не техпроцесс]";
+
+        var техпроцесс = (ТехнологическийПроцесс)объект;
+        return техпроцесс.Обозначение + " _ " + техпроцесс.Наименование;
+    }
+}
